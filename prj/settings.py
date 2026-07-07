72fd0765357dd06636b122dd3b7be8ed73952c67
kajakgm <kajakgm@gmail.com>
2026-07-07 19:59:36 +0530

    Update settings.py

--- prj/settings.py
+++ prj/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -114,12 +115,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
 import os
 STATIC_URL = 'static/'
 STATIC_ROOT=BASE_DIR /'static'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-
+STORAGES={
+    "staticfiles":{"BACKEND":"whitenoise.storage.CompressedManifestStaticFilesStorage",},}
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
